860f2e8a6c2a2c5aaafe59f7688d79f69d521766
minicastle11 <130207045+minicastle11@users.noreply.github.com>
2023-05-06 22:21:41 +0900

    Create crud.h

---
+++ crud.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct{
+    int exerciseTime;
+    int majorStudy;
+    int minorStudy;
+    int sleepTime;
+    int eatingCount;
+    int readingTime;
+    int friendshipTimme;
+}standard;
+
+typedef struct{
+    int exerciseTime;
+    int majorStudy;
+    int minorStudy;
+    int sleepTime;
+    int eatingCount;
+    int readingTime;
+    int friendshipTimme;
+}daily;
